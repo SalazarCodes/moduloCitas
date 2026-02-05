dec15207e11dc1b94061a6324427301cfd642049
glb_lsalazar <glb_lsalazar@niubiz.com.pe>
2026-02-05 16:23:10 -0500

    fix: remover useEffect no usado

--- src/modals/PaymentModal.jsx
+++ src/modals/PaymentModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { X, CreditCard, Banknote, FileText, Save } from 'lucide-react';
 import { TRATAMIENTOS } from '../constants/salonData';
 
